4a110016089b2085a8031bbf071ea8d6f325e547
Serhii Kamets <sergiokamets@gmail.com>
2023-01-02 16:50:50 +0100

    Remove consol logs

--- src/js/controller.js
+++ src/js/controller.js
@@ -113,7 +113,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome back!');
 };
 
 const clearBookmarks = function () {
