7297afde34e92d31023abe2cf7eb6703dab4ef14
Serhii Kamets <sergiokamets@gmail.com>
2023-01-02 16:13:35 +0100

    Check netify connection with git

--- src/js/controller.js
+++ src/js/controller.js
@@ -113,6 +113,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('Welcome back!');
 };
 
 const clearBookmarks = function () {
